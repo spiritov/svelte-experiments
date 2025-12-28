0f17657f5eefd032a53372dbc53f0d850b25b30f
bailey <spiritov_v@pm.me>
2025-12-27 20:19:58 -0700

    add Table

---
+++ src/lib/components/display/table/Table.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import type { Snippet } from 'svelte';
+  import { fade } from 'svelte/transition';
+
+  type Props = {
+    data: object[];
+    header: Snippet;
+    row: Snippet<[object]>;
+  };
+
+  let { data, header, row }: Props = $props();
+</script>
+
+<table class="border-separate border-spacing-y-0.5 rounded-layout text-sm">
+  <!-- header -->
+  <thead class="text-primary/75">
+    <tr class="truncate">
+      <!-- <th class="w-number">{content}</th> -->
+      {@render header()}
+    </tr>
+  </thead>
+
+  <!-- rows -->
+  <tbody>
+    {#each data as object}
+      <tr transition:fade class="my-1 truncate">
+        {@render row(object)}
+        <!-- <td>{content}</td> -->
+      </tr>
+    {/each}
+  </tbody>
+</table>
