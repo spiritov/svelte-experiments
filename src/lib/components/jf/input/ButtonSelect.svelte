92465c232fd81070d65cd14963c537ed298e1dd5
bailey <spiritov_v@pm.me>
2025-12-27 22:10:55 -0700

    update ButtonSelect

--- src/lib/components/jf/input/ButtonSelect.svelte
+++ src/lib/components/jf/input/ButtonSelect.svelte
@@ -25,28 +25,41 @@
     {#each options as { value, src }}
       {@const isSelected: boolean = selected === value}
       <button
-        class="rounded-box {isSelected ? 'bg-base-900' : 'bg-base-800'}"
+        class="rounded-box {isSelected ? 'bg-base-900' : 'bg-base-800'} group"
         onclick={() => {
           onsubmit(value);
           selected = value;
         }}>
-        <img class="w-20 px-2" {src} alt="" draggable="false" />
+        <img
+          class="w-20 px-2 group-hover:opacity-100 {isSelected ? '' : 'opacity-50'}"
+          {src}
+          alt=""
+          draggable="false" />
       </button>
     {/each}
     <!-- reset option -->
     {#if withNone}
-      {@const noneSelected: boolean = selected === 'None'}
+      {@const isSelected: boolean = selected === 'None'}
       <!-- svelte-ignore a11y_consider_explicit_label -->
       <button
-        class="grid w-20 place-content-center rounded-box {noneSelected
-          ? 'bg-base-900'
-          : 'bg-base-800'}"
+        class="group grid w-20 place-content-center rounded-box
+        {isSelected ? 'bg-base-900' : 'bg-base-800'}"
         onclick={() => {
           onsubmit('None');
           selected = 'None';
         }}>
-        <span class="icon-[mdi--close] size-6"></span>
+        <span
+          class="icon-[mdi--close] size-6 group-hover:opacity-100 {isSelected ? '' : 'opacity-50'}"
+        ></span>
       </button>
     {/if}
   </div>
 </label>
+
+<style>
+  /* match theme content color */
+  img {
+    filter: brightness(0) saturate(100%) invert(97%) sepia(49%) saturate(6023%) hue-rotate(179deg)
+      brightness(100%) contrast(108%);
+  }
+</style>
