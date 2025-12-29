788b2bee7e1e38d76d44043c82eb2e3e41c4dcde
bailey <spiritov_v@pm.me>
2025-12-29 16:55:03 -0700

    update Header

--- src/lib/components/jf/display/profile/Header.svelte
+++ src/lib/components/jf/display/profile/Header.svelte
@@ -22,16 +22,16 @@
   class="absolute top-0 left-0 flex h-64 w-full flex-col overflow-hidden rounded-t-layout bg-base-900">
   <!-- absolute avatar -->
   <img
-    class="absolute top-1/3 left-8 z-10 size-32 rounded-layout drop-shadow-md/50"
+    class="absolute top-1/3 left-6 z-10 size-32 rounded-layout drop-shadow-md/50"
     src={player.steam_avatar_url}
     alt="" />
   <!-- absolute display name -->
-  <div class="absolute top-36 left-0 z-10 ml-44">
+  <div class="absolute top-36 left-0 z-10 ml-42">
     <span class="rounded-layout bg-base-900 px-2 py-1 pb-12 text-2xl text-primary"
       >{player.display_name}</span>
   </div>
   <!-- absolute bottom, class select & points -->
-  <div class="absolute right-8 bottom-1 z-20 flex flex-row-reverse items-end">
+  <div class="absolute right-6 bottom-1 z-20 flex flex-row-reverse items-end">
     <div class="flex">
       <ClassSelect
         selected={player.preferred_class}
@@ -54,7 +54,7 @@
     src="https://tempusplaza.xyz/map-backgrounds/{'jump_escape_rc4'}.jpg"
     alt="" />
   <!-- middle, country & division -->
-  <div class="relative z-10 ml-40 h-1/6 px-3 pt-3">
+  <div class="relative z-10 ml-38 h-1/6 px-3 pt-3">
     <div class="flex items-center gap-1 text-content/75">
       <Flag code={player.country_code} country={player.country} />
       <span class="icon-[mdi--circle-medium] size-3.5"></span>
@@ -71,10 +71,10 @@
     </div>
   </div>
   <!-- bottom, links -->
-  <div class="ml-8 flex grow items-center gap-2 text-sm">
+  <div class="ml-6 flex grow items-center gap-2 text-sm">
     <Link label="Tempus" src={tempus} href={`https://tempus2.xyz/players/${player.tempus_id}`} />
     <Link label="Plaza" src={plaza} href={`https://tempusplaza.xyz/players/${player.tempus_id}`} />
   </div>
 </div>
 
-<hr class="h-58 w-full" />
+<hr class="invisible mt-4 h-64 w-full" />
--- src/lib/components/jf/display/profile/Points.svelte
+++ src/lib/components/jf/display/profile/Points.svelte
@@ -6,15 +6,15 @@
   let { points }: Props = $props();
 </script>
 
-<div class="flex gap-2">
+<div class="flex gap-4">
   {@render category('last 3 monthly', points.last_3_monthly)}
   {@render category('last 9 motw', points.last_9_motw)}
   {@render category('total', points.total)}
 </div>
 
 {#snippet category(label: string, value: number)}
-  <div class="flex h-fit min-w-24 flex-col bg-base-700/0">
-    <span class="text-sm text-content/75">{label}</span>
+  <div class="flex h-fit min-w-20 flex-col bg-base-700/0">
+    <span class="text-sm font-bold text-content/75">{label}</span>
     <span>{value}</span>
   </div>
 {/snippet}
